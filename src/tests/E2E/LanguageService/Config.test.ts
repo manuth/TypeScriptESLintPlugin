35354404f610bd02bba2373ad86115b89dc169c5
Manuel Thalmann <m@nuth.ch>
2020-03-14 18:54:24 +0100

    Disable timeouts for drone

--- src/tests/E2E/LanguageService/Config.test.ts
+++ src/tests/E2E/LanguageService/Config.test.ts
@@ -9,8 +9,9 @@ export = (tester: LanguageServiceTester): void =>
         () =>
         {
             teardown(
-                async () =>
+                async function()
                 {
+                    this.enableTimeouts(false);
                     await tester.Configure({});
                 });
 
