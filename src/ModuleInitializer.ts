391b23d992bded4b51eb04ee7c446b1e907b5668
Manuel Thalmann <m@nuth.ch>
2020-03-18 22:40:43 +0100

    Add an error-message if the version is invalid.

--- src/ModuleInitializer.ts
+++ src/ModuleInitializer.ts
@@ -1,6 +1,8 @@
 import { IInitializationOptions } from "./IInitializationOptions";
 import ts = require("typescript/lib/tsserverlibrary");
 import { PluginModule } from "./PluginModule";
+import { Constants } from "./Constants";
+import { Interceptor } from "./Interception/Interceptor";
 
 /**
  * Provides the functionality to initialize new `PluginModule`s.
@@ -25,9 +27,30 @@ export class ModuleInitializer
         else
         {
             return {
-                create(pluginCreateInfo): ts.LanguageService
+                create(createInfo): ts.LanguageService
                 {
-                    return pluginCreateInfo.languageService;
+                    let interceptor = new Interceptor(createInfo.languageService, true);
+                    interceptor.AddMethod(
+                        "getSemanticDiagnostics",
+                        (target, delegate, fileName) =>
+                        {
+                            let diagnostics = delegate(fileName);
+
+                            diagnostics.push(
+                                {
+                                    source: Constants.ErrorSource,
+                                    code: Constants.ErrorCode,
+                                    category: ts.DiagnosticCategory.Error,
+                                    file: target.getProgram().getSourceFile(fileName),
+                                    start: 0,
+                                    length: 0,
+                                    messageText: "Invalid TypeScript version detected. The `typescript-eslint-plugin` requires TypeScript 3.0 or higher."
+                                });
+
+                            return diagnostics;
+                        });
+
+                    return createInfo.languageService;
                 }
             };
         }
