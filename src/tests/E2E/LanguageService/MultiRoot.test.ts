3c4a28e78a1c150071d7ce91be0b7a68bb43a0ec
Manuel Thalmann <m@nuth.ch>
2020-04-05 22:41:30 +0200

    Create a new test-server for each test-suite

--- src/tests/E2E/LanguageService/MultiRoot.test.ts
+++ src/tests/E2E/LanguageService/MultiRoot.test.ts
@@ -1,30 +1,41 @@
 import Assert = require("assert");
 import { LanguageServiceTester } from "./LanguageServiceTester";
 
-export = (tester: LanguageServiceTester): void =>
-{
-    suite(
-        "Multi-Root",
-        () =>
-        {
-            test(
-                "Checking whether files from foreign directories are processed correctly…",
-                async function()
-                {
-                    let code = `
-                        debugger;
-                        /abc[]/.test("abcd");`;
+suite(
+    "Multi-Root",
+    () =>
+    {
+        let tester: LanguageServiceTester;
 
-                    this.enableTimeouts(false);
-                    let foreignFileName = tester.TSServer.MakePath("..", "workspace-2", "src", "typescript.ts");
-                    let charClassRule = "no-empty-character-class";
-                    let debuggerRule = "no-debugger";
-                    let response = await tester.AnalyzeCode(code);
-                    Assert.ok(response.Filter(charClassRule).length > 0);
-                    Assert.strictEqual(response.Filter(debuggerRule).length, 0);
-                    response = await tester.AnalyzeCode(code, "TS", foreignFileName);
-                    Assert.ok(response.Filter(debuggerRule).length > 0);
-                    Assert.strictEqual(response.Filter(charClassRule).length, 0);
-                });
-        });
-};
+        suiteSetup(
+            () =>
+            {
+                tester = new LanguageServiceTester();
+            });
+
+        suiteTeardown(
+            () =>
+            {
+                tester.Dispose();
+            });
+
+        test(
+            "Checking whether files from foreign directories are processed correctly…",
+            async function()
+            {
+                let code = `
+                    debugger;
+                    /abc[]/.test("abcd");`;
+
+                this.enableTimeouts(false);
+                let foreignFileName = tester.TSServer.MakePath("..", "workspace-2", "src", "typescript.ts");
+                let charClassRule = "no-empty-character-class";
+                let debuggerRule = "no-debugger";
+                let response = await tester.AnalyzeCode(code);
+                Assert.ok(response.Filter(charClassRule).length > 0);
+                Assert.strictEqual(response.Filter(debuggerRule).length, 0);
+                response = await tester.AnalyzeCode(code, "TS", foreignFileName);
+                Assert.ok(response.Filter(debuggerRule).length > 0);
+                Assert.strictEqual(response.Filter(charClassRule).length, 0);
+            });
+    });
--- src/tests/E2E/LanguageService/index.ts
+++ src/tests/E2E/LanguageService/index.ts
@@ -1,19 +1,9 @@
-import DiagnosticsTests = require("./Diagnostics.test");
-import ConfigTests = require("./Config.test");
-import { LanguageServiceTester } from "./LanguageServiceTester";
-import MultiRootTests = require("./MultiRoot.test");
-
 suite(
     "Language-Service",
     () =>
     {
-        /**
-         * A component for testing the language-service.
-         */
-        let tester: LanguageServiceTester = new LanguageServiceTester();
-        suiteTeardown(async () => tester.Dispose());
         require("./General.test");
-        DiagnosticsTests(tester);
-        ConfigTests(tester);
-        MultiRootTests(tester);
+        require("./Diagnostics.test");
+        require("./Config.test");
+        require("./MultiRoot.test");
     });
