586fa83c8a67d65229a122be77e6b1477ef009bc
Manuel Thalmann <m@nuth.ch>
2020-04-06 16:53:17 +0200

    Create a logger class with fixed log level

---
+++ src/Logging/Logger.ts
@@ -0,0 +1,40 @@
+import { Plugin } from "../Plugin";
+import { LogLevel } from "./LogLevel";
+import { PluginLogger } from "./PluginLogger";
+
+/**
+ * Provides the functionality to print log messages according to the specified log-level.
+ */
+export class Logger extends PluginLogger
+{
+    /**
+     * The log-level to print.
+     */
+    private logLevel: LogLevel;
+
+    /**
+     * Initializes a new instance of the `Logger` class.
+     *
+     * @param plugin
+     * The plugin.
+     *
+     * @param logLevel
+     * The log-level to print.
+     *
+     * @param category
+     * The category of the logger.
+     */
+    public constructor(plugin: Plugin, logLevel: LogLevel = LogLevel.Info, category?: string)
+    {
+        super(plugin, category);
+        this.logLevel = logLevel;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public get LogLevel(): LogLevel
+    {
+        return this.logLevel;
+    }
+}
