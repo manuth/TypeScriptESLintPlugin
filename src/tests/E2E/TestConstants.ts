7486e58810015602702f69ede6bf43b761ec2374
Manuel Thalmann <m@nuth.ch>
2020-09-23 16:37:33 +0200

    Add important information to the TestConstants

--- src/tests/E2E/TestConstants.ts
+++ src/tests/E2E/TestConstants.ts
@@ -1,3 +1,4 @@
+import { Package } from "@manuth/package-json-editor";
 import { join } from "upath";
 
 /**
@@ -6,17 +7,40 @@ import { join } from "upath";
 export class TestConstants
 {
     /**
+     * Gets the path to the directory of this package.
+     */
+    public static readonly PackageDirectory = join(__dirname, "..", "..", "..");
+
+    /**
      * Gets the path whoch contains files for testing.
      */
     public static readonly TestDirectory = join(__dirname, "..", "..", "..", "test");
 
     /**
-     * Gets the path to the project-directory.
-     */
-    public static readonly ProjectDirectory = join(TestConstants.TestDirectory, "workspace");
-
-    /**
      * Gets the path to save temporary workspaces to.
      */
     public static readonly TempWorkspaceDirectory = join(TestConstants.TestDirectory, "temp-workspaces");
+
+    /**
+     * Gets the path to the project-directory.
+     */
+    public static readonly ProjectDirectory = join(TestConstants.TempWorkspaceDirectory, "main");
+
+    /**
+     * An object which represents this package.
+     */
+    private static package: Package = null;
+
+    /**
+     * Gets an object which represents this package.
+     */
+    public static get Package(): Package
+    {
+        if (this.package === null)
+        {
+            this.package = new Package(join(this.PackageDirectory, "package.json"));
+        }
+
+        return this.package;
+    }
 }
