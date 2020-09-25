a3e61acba2cec2734c6c8f621d4d35835f2f0da7
Manuel Thalmann <m@nuth.ch>
2020-09-25 13:06:15 +0200

    Normalize and simplify the config-tests

--- src/tests/E2E/LanguageService/Config.test.ts
+++ src/tests/E2E/LanguageService/Config.test.ts
@@ -15,11 +15,21 @@ export function ConfigTests(): void
         () =>
         {
             let tester: LanguageServiceTester;
+            let correctCode: string;
+            let incorrectCode: string;
+            let ruleFailureCode: string;
+            let ruleName: string;
+            let disableLineComment: string;
 
             suiteSetup(
                 () =>
                 {
                     tester = LanguageServiceTester.Default;
+                    correctCode = "";
+                    incorrectCode = "let x: sting";
+                    ruleFailureCode = 'console.log("Hello World");  ';
+                    ruleName = "no-trailing-spaces";
+                    disableLineComment = `/* eslint-disable-next-line ${ruleName} */`;
                 });
 
             teardown(
@@ -27,6 +37,12 @@ export function ConfigTests(): void
                 {
                     this.timeout(0);
                     await tester.ConfigurePlugin({});
+                    await tester.Configure();
+
+                    await tester.Configure(
+                        {
+                            [ruleName]: "warn"
+                        });
                 });
 
             test(
@@ -34,15 +50,14 @@ export function ConfigTests(): void
                 async function()
                 {
                     this.timeout(0);
-                    let code = 'console.log("Hello World");;;';
-                    let diagnosticsResponse = await tester.AnalyzeCode(code, "JS");
+                    let diagnosticsResponse = await tester.AnalyzeCode(ruleFailureCode, "JS");
                     Assert.ok(diagnosticsResponse.Diagnostics.length > 0);
-                    diagnosticsResponse = await tester.AnalyzeCode(code, "JSX");
+                    diagnosticsResponse = await tester.AnalyzeCode(ruleFailureCode, "JSX");
                     Assert.ok(diagnosticsResponse.Diagnostics.length > 0);
                     await tester.ConfigurePlugin({ ignoreJavaScript: true });
-                    diagnosticsResponse = await tester.AnalyzeCode(code, "JS");
+                    diagnosticsResponse = await tester.AnalyzeCode(ruleFailureCode, "JS");
                     Assert.strictEqual(diagnosticsResponse.Diagnostics.length, 0);
-                    diagnosticsResponse = await tester.AnalyzeCode(code, "JSX");
+                    diagnosticsResponse = await tester.AnalyzeCode(ruleFailureCode, "JSX");
                     Assert.strictEqual(diagnosticsResponse.Diagnostics.length, 0);
                 });
 
@@ -51,15 +66,14 @@ export function ConfigTests(): void
                 async function()
                 {
                     this.timeout(0);
-                    let code = 'console.log("Hello World")';
-                    let diagnosticsResponse = await tester.AnalyzeCode(code, "TS");
+                    let diagnosticsResponse = await tester.AnalyzeCode(ruleFailureCode, "TS");
                     Assert.ok(diagnosticsResponse.Diagnostics.length > 0);
-                    diagnosticsResponse = await tester.AnalyzeCode(code, "TSX");
+                    diagnosticsResponse = await tester.AnalyzeCode(ruleFailureCode, "TSX");
                     Assert.ok(diagnosticsResponse.Diagnostics.length > 0);
                     await tester.ConfigurePlugin({ ignoreTypeScript: true });
-                    diagnosticsResponse = await tester.AnalyzeCode(code, "TS");
+                    diagnosticsResponse = await tester.AnalyzeCode(ruleFailureCode, "TS");
                     Assert.strictEqual(diagnosticsResponse.Diagnostics.length, 0);
-                    diagnosticsResponse = await tester.AnalyzeCode(code, "TSX");
+                    diagnosticsResponse = await tester.AnalyzeCode(ruleFailureCode, "TSX");
                     Assert.strictEqual(diagnosticsResponse.Diagnostics.length, 0);
                 });
 
@@ -67,9 +81,8 @@ export function ConfigTests(): void
                 "Checking whether inline-config can be prohibited…",
                 async function()
                 {
-                    let ruleName = "spaced-comment";
-                    let code = `/* eslint-disable ${ruleName} */
-                                let x = "hello world"; //who KnoWs how To formAt cOmmENts?`;
+                    let code = `${disableLineComment}
+                                ${ruleFailureCode}`;
                     this.timeout(0);
                     let response = await tester.AnalyzeCode(code);
                     Assert.strictEqual(response.Filter(ruleName).length, 0);
@@ -83,7 +96,7 @@ export function ConfigTests(): void
                 async function()
                 {
                     this.timeout(0);
-                    let code = "/* eslint-disable-next-line no-trailing-spaces */\n";
+                    let code = `${disableLineComment}\n`;
 
                     /**
                      * Checks whether at least one error-message for unnecessary `eslint-disable` directives is reported.
@@ -101,8 +114,7 @@ export function ConfigTests(): void
                             {
                                 if ("text" in diagnostic)
                                 {
-                                    return !(/\([a-z-]*\)$/.test(diagnostic.text)) &&
-                                        /\Weslint-disable\W/.test(diagnostic.text) &&
+                                    return /\Weslint-disable\W/.test(diagnostic.text) &&
                                         /unused/i.test(diagnostic.text);
                                 }
                                 else
@@ -146,8 +158,6 @@ export function ConfigTests(): void
                 "Checking whether all diagnostics can be changed to warnings…",
                 async function()
                 {
-                    let ruleName = "no-extra-semi";
-                    let code = 'let name = "John";;;';
                     this.timeout(0);
 
                     /**
@@ -167,17 +177,20 @@ export function ConfigTests(): void
                         return diagnostics.every((diagnostic) => diagnostic.category === errorLevel);
                     };
 
-                    Assert.ok(hasErrorLevel((await tester.AnalyzeCode(code)).Filter(ruleName), "error"));
+                    await tester.Configure({ [ruleName]: "error" });
+                    Assert.ok(hasErrorLevel((await tester.AnalyzeCode(ruleFailureCode)).Filter(ruleName), "error"));
                     await tester.ConfigurePlugin({ alwaysShowRuleFailuresAsWarnings: true });
-                    Assert.ok(hasErrorLevel((await tester.AnalyzeCode(code)).Filter(ruleName), "warning"));
+                    Assert.ok(hasErrorLevel((await tester.AnalyzeCode(ruleFailureCode)).Filter(ruleName), "warning"));
                 });
 
             test(
                 "Checking whether eslint can be disabled when other errors are present…",
                 async () =>
                 {
-                    let code = "let x: Array<sting>;  ";
-                    let ruleName = "no-trailing-spaces";
+                    let code = `
+                        ${ruleFailureCode}
+                        ${incorrectCode}`;
+
                     Assert.ok((await tester.AnalyzeCode(code)).Filter(ruleName).length > 0);
                     await tester.ConfigurePlugin({ suppressWhileTypeErrorsPresent: true });
                     Assert.strictEqual((await tester.AnalyzeCode(code)).Filter(ruleName).length, 0);
@@ -187,8 +200,6 @@ export function ConfigTests(): void
                 "Checking whether warnings about the use of deprecated rules can be suppressed…",
                 async function()
                 {
-                    let code = 'console.log("this code is correct");\n';
-
                     let workspace = await tester.CreateTemporaryWorkspace(
                         {
                             "prefer-reflect": "warn"
@@ -221,9 +232,9 @@ export function ConfigTests(): void
                     };
 
                     this.timeout(0);
-                    Assert.ok(deprecatedRuleDetector(await workspace.AnalyzeCode(code)));
+                    Assert.ok(deprecatedRuleDetector(await workspace.AnalyzeCode(correctCode)));
                     await workspace.ConfigurePlugin({ suppressDeprecationWarnings: true });
-                    Assert.ok(!deprecatedRuleDetector(await workspace.AnalyzeCode(code)));
+                    Assert.ok(!deprecatedRuleDetector(await workspace.AnalyzeCode(correctCode)));
                 });
 
             test(
@@ -274,9 +285,9 @@ export function ConfigTests(): void
                             cwd: workspace.MakePath()
                         });
 
-                    Assert.ok(!eslintConfigErrorDetector(await workspace.AnalyzeCode("")));
+                    Assert.ok(!eslintConfigErrorDetector(await workspace.AnalyzeCode(correctCode)));
                     await workspace.ConfigurePlugin({ suppressConfigNotFoundError: false });
-                    Assert.ok(eslintConfigErrorDetector(await workspace.AnalyzeCode("")));
+                    Assert.ok(eslintConfigErrorDetector(await workspace.AnalyzeCode(correctCode)));
                 });
 
             test(
@@ -284,19 +295,15 @@ export function ConfigTests(): void
                 async function()
                 {
                     this.timeout(0);
-                    let code = "    ";
-                    let ruleName = "no-trailing-spaces";
 
                     let workspace = await tester.CreateTemporaryWorkspace(
-                        {
-                            [ruleName]: "warn"
-                        },
+                        null,
                         {
                             ignoreJavaScript: false
                         });
 
                     await tester.ConfigurePlugin({ ignoreJavaScript: true });
-                    let response = await workspace.AnalyzeCode(code, "JS");
+                    let response = await workspace.AnalyzeCode(ruleFailureCode, "JS");
                     Assert.ok(response.Filter(ruleName).length > 0);
                 });
         });
